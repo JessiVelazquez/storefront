b2107bf2438cc3fc8b749cd1478bd18b2cd3cf81
Jessi <jessivelazq1@gmail.com>
2021-06-08 17:22:12 -0700

    incrementing cart total

--- src/app.js
+++ src/app.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import './styles/style.scss';
 
 import Categories from './components/categories.js';
 import Products from './components/products.js';
 import Header from './components/header.js';
 import Footer from './components/footer.js';
+import SimpleCart from './components/simplecart.js';
 
 function App() {
   return (
     <>
       <Header />
+      {/* <SimpleCart /> */}
       <Categories />
       <Products />
       <Footer />
---
+++ src/components/simplecart.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { catChange, reset } from '../store/products.js';
+import { increment } from '../store/simplecart.js';
+
+function SimpleCart(props) {
+  return (
+    <section>
+      <p>{props.cartReducer.cartItems}</p>
+    </section>
+  )
+}
+
+const mapStateToProps = state => ({
+  prodReducer: state.prodReducer,
+  catReducer: state.catReducer,
+  cartReducer: state.cartReducer
+})
+
+const mapDispatchToProps = dispatch => ({
+  catChange: (name) => dispatch(catChange(name)),
+  reset: () => dispatch(reset()),
+  increment: (product) => dispatch(increment(product))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(SimpleCart);
