72aff1292502a172722c4a8b13406ef4f2fe44fb
Jessi <jessivelazq1@gmail.com>
2021-06-11 15:18:25 -0700

    my own api set up and getting data

--- src/store/api-actions.js
+++ src/store/api-actions.js
@@ -1,15 +1,14 @@
 import superagent from 'superagent';
 
-// let api = 'https://jessi-api-server.herokuapp.com/store';
+let api = 'https://jessi-api-server.herokuapp.com/store';
 
 //cokos API
-let api = 'https://api-js401.herokuapp.com/api/v1/products'; 
+// let api = 'https://api-js401.herokuapp.com/api/v1/products'; 
 
 export const getRemoteData = () => dispatch => {
   return superagent.get(api)
   .then(response => {
     dispatch(getAction(response.body))
-    console.log('GET RESP--', response.body);
   })
 }
 
